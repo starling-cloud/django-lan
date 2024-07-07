4e55c7b92ec13f4732bf6a8115cd80e2fc132d01
Lars van Vianen <lars@vianen.com>
2024-07-07 17:19:26 +0200

    Create field_model_port_number.py

---
+++ src/django-lan/fields/model/field_model_port_number.py
@@ -0,0 +1,97 @@
+# -*- coding: utf-8 -*-
+
+
+# =============================================================================
+# Docstring
+# =============================================================================
+
+"""
+Provides Port Number Model Field Class
+======================================
+
+A custom field to ensure that the port numbers are within the valid
+range (1-65535).
+
+Links:
+- https://en.wikipedia.org/wiki/Port_(computer_networking)
+
+"""
+
+
+# =============================================================================
+# Import
+# =============================================================================
+
+# Import | Standard Library
+from typing import Any
+
+# Import | Libraries
+from django.db.models import IntegerField
+from django.utils.translation import gettext_lazy as _
+
+# Import | Local Modules
+from ...utils.validators.validator_port_number import validate_port_number
+
+
+# =============================================================================
+# Variables
+# =============================================================================
+
+
+# =============================================================================
+# Classes
+# =============================================================================
+
+class PortNumberModelField(IntegerField):
+    """
+    Port Number Model Field Class
+    =============================
+
+    A Django model field that stores and validates network port numbers.
+
+    Attributes:
+        description (str): Description of what the field is used for.
+
+    """
+
+    # Class | Variables
+    # =========================================================================
+
+    description = _(
+        "A field that stores and validates network port numbers."
+    )
+
+    # Class | Methods
+    # =========================================================================
+
+    def __init__(self, *args: Any, **kwargs: Any) -> None:
+        """
+        Initializes the PortNumberModelField with custom validation.
+        """
+        super().__init__(*args, **kwargs)
+        self.validators.append(validate_port_number)
+
+    def clean(self, value: Any, model_instance: Any) -> Any:
+        """
+        Cleans and validates the input value using the superclass’s
+        cleaning logic.
+
+        Args:
+            value (Any): The value that needs to be cleaned.
+            model_instance (Any): The Django model instance that this field
+                belongs to.
+
+        Returns:
+            Any: The cleaned and validated value.
+        """
+        # Validator will handle port range check
+        return super().clean(value, model_instance)
+
+
+# =============================================================================
+# Public Interface
+# =============================================================================
+
+__all__ = [
+    "PortNumberModelField",
+]
