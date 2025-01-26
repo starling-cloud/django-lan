7f5ffa67f55b51a63d981608a49da23d509d06de
Lars van Vianen <lars@vianen.com>
2025-01-26 11:10:25 +0100

    Update ip_to_binary.py

--- src/django_lan/utils/converters/ip_to_binary.py
+++ src/django_lan/utils/converters/ip_to_binary.py
@@ -1,10 +1,12 @@
+"""
 Convert IP Address to Binary
 A utility function that converts an IP address into its binary representation, useful for subnetting and network calculations.
-
+"""
 
 import ipaddress
 
-def ip_to_binary(ip_address):
+
+def ip_to_binary(ip_address) -> str:
     """
     Converts an IP address to its binary representation.
 
@@ -14,5 +16,5 @@ def ip_to_binary(ip_address):
     Returns:
     str: The binary representation of the IP address.
     """
-    ip = ipaddress.ip_address(ip_address)
-    return format(int(ip), '0>128b') if ip.version == 6 else format(int(ip), '0>32b')
+    ip: ipaddress.IPv4Address | ipaddress.IPv6Address = ipaddress.ip_address(address=ip_address)
+    return format(int(ip), format_spec='0>128b') if ip.version == 6 else format(int(x=ip), format_spec='0>32b')
