046d5287a7b730a343244ca650753755fa009d38
Lars van Vianen <lars@vianen.com>
2025-02-01 03:51:23 +0100

    Update cidr_to_subnet_mask.py

--- src/django_lan/utils/converters/cidr_to_subnet_mask.py
+++ src/django_lan/utils/converters/cidr_to_subnet_mask.py
@@ -1,10 +1,12 @@
+"""
 Convert CIDR to Subnet Mask
 A function to convert CIDR notation to a subnet mask, useful for network configuration tasks.
-
+"""
 
 import ipaddress
 
-def cidr_to_subnet_mask(cidr):
+
+def cidr_to_subnet_mask(cidr) -> str:
     """
     Converts CIDR notation to a subnet mask.
 
@@ -14,5 +16,5 @@ def cidr_to_subnet_mask(cidr):
     Returns:
     str: The corresponding subnet mask.
     """
-    network = ipaddress.ip_network(cidr, strict=False)
-    return str(network.netmask)
+    network: ipaddress.IPv4Network | ipaddress.IPv6Network = ipaddress.ip_network(address=cidr, strict=False,)
+    return str(object=network.netmask)
